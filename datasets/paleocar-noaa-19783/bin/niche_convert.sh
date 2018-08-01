2ba1869febd96478e8e4ec8f45988238bbcf8ddd
Jeff Terstriep <jefft@illinois.edu>
2018-08-01 21:39:17 +0000

    Fixed niche_convert.sh which doesn't use nc4 extension and add comments.

--- datasets/paleocar-noaa-19783/bin/niche_convert.sh
+++ datasets/paleocar-noaa-19783/bin/niche_convert.sh
@@ -5,20 +5,20 @@
 # not uniformly named in paleocar datasets, use "Year" for GDD
 # and PPT and "Year AD" for niche.
 
-dst=/projects/skope/datasets/paleocar_2/maize_farming_niche
+dst=/projects/skope/datasets/paleocar_2/maize_farming_niche/geoserver2
 var="NICHE"
 
 for y in {0..1999}; do
   year=`printf '%04d' $((y+1))`
 
-  rm data/*${var}_????.nc4 > /dev/null
+  rm data/*${var}_????.nc > /dev/null
   for f in $@; do
-    ncks -d "Year AD",$y ${f} ${f/.nc4/_${year}.nc4}
-    echo ${f/.nc4/_${year}.nc4}
+    ncks -d "Year AD",$y ${f} ${f/.nc/_${year}.nc}
+    echo ${f/.nc/_${year}.nc}
   done
 
-  gdal_merge.py -init -32768 -a_nodata -32768 -o ${dst}/${var}_${year}.tif data/*${var}_????.nc4
-  rm data/*${var}_????.nc4
+  gdal_merge.py -init -32768 -a_nodata -32768 -a_src EPSG:4326 -o ${dst}/${var}_${year}.tif data/*${var}_????.nc
+  rm data/*${var}_????.nc
   echo ${year}   `date`
 done
-rm data/*${var}_????.nc4
+rm data/*${var}_????.nc
