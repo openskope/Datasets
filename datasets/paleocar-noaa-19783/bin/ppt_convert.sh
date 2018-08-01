edbb2ca81fcefdbca2b861e2dd46bb2417dbef71
Jeff Terstriep <jefft@illinois.edu>
2018-08-01 18:39:00 +0000

    Add scripts to convert PaleoCAR tiled netCDF files to GeoTiff files.

---
+++ datasets/paleocar-noaa-19783/bin/ppt_convert.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# usage: bash xxx_convert.sh file [file [file]]
+#
+# use 'ncks <filename>' to inspect the netcdf file. The time dimension is
+# not uniformly named in paleocar datasets, use "Year" for GDD
+# and PPT and "Year AD" for niche.
+
+dst=/projects/skope/datasets/paleocar_2/water_year_precipitation/geoserver2
+var="PPT"
+
+for y in {0..1999}; do
+  year=`printf '%04d' $((y+1))`
+
+  rm data/*${var}_????.nc4 > /dev/null
+  for f in $@; do
+    ncks -d Year,$y ${f} ${f/.nc4/_${year}.nc4}
+    echo ${f/.nc4/_${year}.nc4}
+  done
+
+  gdal_merge.py -init -32768 -a_nodata -32768 -o ${dst}/${var}_${year}.tif data/*${var}_????.nc4
+  echo ${year}   `date`
+done
+rm data/*${var}_????.nc4
